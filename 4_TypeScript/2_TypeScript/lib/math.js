15688e0757e799a1e344af3012653b85fa7ac6cb
Dan Gartner <dgartner@gmail.com>
2016-04-04 11:41:07 -0500

    Reset to initial state

--- 4_TypeScript/2_TypeScript/lib/math.js
+++ 4_TypeScript/2_TypeScript/lib/math.js
@@ -1,4 +1,15 @@
 ﻿
+/** @description Determines the area of a circle that has the specified radius parameter.
+ * @param {number} radius The radius of the circle.
+ * @return {number}
+ */
+function getArea(radius) {
+    var areaVal;
+    areaVal = Math.PI * radius * radius;
+    return areaVal;
+}
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
--- 4_TypeScript/2_TypeScript/scratch.js
+++ 4_TypeScript/2_TypeScript/scratch.js
@@ -1,4 +1,4 @@
-﻿Object.defineProperty(window, "MySweetApp", { value: "v1.0.0", writable: true });
+﻿Object.defineProperty(window, "MySweetApp", { value: "v1.0.0", readonly: true });
 
 function deliveryMethod() {
     // TODO
@@ -6,24 +6,24 @@ function deliveryMethod() {
 }
 
 function shipWeight() {
-    return parseInt(document.getElementById('weight').textContent);
+    return parseInt(document.getElementById('weight'));
 }
 
 /*
  * @param {(string | string[])} emailAddr - An email address of array of email addresses
  */
 function sendUpdates(emailAddr) {
-    function sendEmail(addr: (string | string[])) {
+    function sendEmail(addr) {
         // Default to standard delivery if empty
         console.log(`Shipping to ${addr} via ${deliveryMethod() | "standard"} delivery`);
 
-        if (shipWeight() > 100) {
+        if (shipWeight > 100) {
             console.log("WARNING: Oversize package");
         }
     }
     // If its an array, loop over it
-    if (Array.isArray(emailAddr.length)) {
-        emailAddr.forEach(( val, idx) => {
+    if (Array.isArray(emailAddr)) {
+        emailAddr.forEach((idx, val) => {
             sendEmail(val.trim());
         });
     } else {
