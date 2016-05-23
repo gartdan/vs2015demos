0d1961dbb1b33f1a16a2bae974b1589f7bfe4f21
Dan Gartner <dgartner@gmail.com>
2016-05-23 12:27:20 -0500

    Implemented TreeWriter

--- 1_DeveloperProductivity/1_DeveloperProductivity/TreeWriter.cs
+++ 1_DeveloperProductivity/1_DeveloperProductivity/TreeWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ namespace DeveloperProductivity
 {
     public class TreeWriter<T> where T : IComparable
     {
-        public void Write(Tree<T> tree, string fileName)
+        public void Write(Tree<T> tree, string path)
         {
-
+            var stream = new FileStream(path, FileMode.OpenOrCreate);
+            var bytes = Encoding.UTF8.GetBytes(tree.ToString());
+            stream.Write(bytes, 0, bytes.Length);
         }
     }
 }
