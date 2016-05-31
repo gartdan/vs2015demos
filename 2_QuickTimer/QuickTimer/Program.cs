69bb1d81413d0f9669663406de847ace7eca6367
Dan Gartner <dgartner@gmail.com>
2016-05-31 10:18:45 -0500

    INITIAL CONDITION for C#6 Demos (again)

--- 2_QuickTimer/QuickTimer/Program.cs
+++ 2_QuickTimer/QuickTimer/Program.cs
@@ -3,6 +3,9 @@ using System.Diagnostics;
 using System.Threading;
 //TODO 1: Demo -- using Static classes
 using static System.Console;
+using static System.Math;
+using static System.DayOfWeek;
+using static System.DateTime;
 
 
 namespace QuickTimer
@@ -21,6 +24,12 @@ namespace QuickTimer
             timer.QuitEvent +=  (o, e) => Console.Clear();
             timer.ResetEvent += (o, e) => Console.Clear();
             timer.TickEvent += (o, e) => Write($"\r{timer.ElapsedMilliseconds.ToSeconds()}");
+
+            if(DateTime.Now.Date.DayOfWeek == DayOfWeek.Saturday || Now.Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("I dont work on the weekend.");
+                return;
+            }
 
             WriteLine("Welcome to {0}. {3}Instructions: {3}Press any key to begin or pause. {3}Press '{1}' to reset. {3}Press '{2}' to quit.",
                 AppName, ResetChar, QuitChar, NewLine);
